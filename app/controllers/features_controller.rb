a2af5dc107517d11b70a7680c0d0a304da2bfd86
Michal Czyz <michalczyz@gmail.com>
2008-12-27 10:59:16 +0100

    refactoring of features controller

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,85 +1,50 @@
 class FeaturesController < ApplicationController
-  # GET /features
-  # GET /features.xml
-  def index
-    @features = Feature.find(:all)
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @features }
-    end
-  end
+  before_filter :get_features, :only => [:index]
+  before_filter :get_feature, :only => [:show, :edit, :update, :destroy]
 
-  # GET /features/1
-  # GET /features/1.xml
-  def show
-    @feature = Feature.find(params[:id])
+  def index; end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @feature }
-    end
-  end
-
-  # GET /features/new
-  # GET /features/new.xml
+  def show; end
+  
   def new
     @feature = Feature.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @feature }
-    end
   end
 
-  # GET /features/1/edit
-  def edit
-    @feature = Feature.find(params[:id])
-  end
-
-  # POST /features
-  # POST /features.xml
+  def edit; end
+  
   def create
     @feature = Feature.new(params[:feature])
-
-    respond_to do |format|
-      if @feature.save
-        flash[:notice] = 'Feature was successfully created.'
-        format.html { redirect_to(@feature) }
-        format.xml  { render :xml => @feature, :status => :created, :location => @feature }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @feature.errors, :status => :unprocessable_entity }
-      end
+    if @feature.save
+      flash[:notice] = 'Feature was successfully created.'
+      redirect_to(@feature)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /features/1
-  # PUT /features/1.xml
   def update
-    @feature = Feature.find(params[:id])
-
-    respond_to do |format|
-      if @feature.update_attributes(params[:feature])
-        flash[:notice] = 'Feature was successfully updated.'
-        format.html { redirect_to(@feature) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @feature.errors, :status => :unprocessable_entity }
-      end
+    if @feature.update_attributes(params[:feature])
+      flash[:notice] = 'Feature was successfully updated.'
+      redirect_to(@feature)
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /features/1
-  # DELETE /features/1.xml
   def destroy
-    @feature = Feature.find(params[:id])
     @feature.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(features_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to features_url
   end
+
+  private
+
+  def get_features
+    @features = Feature.find(:all)
+  end
+  
+  def get_feature
+    @feature = Feature.find(params[:id])
+  end
+  
 end
