68990f9e13a4b70470278495557480e76eb99a91
Michal Czyz <michalczyz@gmail.com>
2009-01-26 17:43:58 +0100

    doc update

--- db/migrate/20081227111012_add_feature_id_to_story.rb
+++
@@ -1,9 +0,0 @@
-class AddFeatureIdToStory < ActiveRecord::Migration
-  def self.up
-    add_column :stories, :feature_id, :integer
-  end
-
-  def self.down
-    remove_column :stories, :feature_id
-  end
-end
