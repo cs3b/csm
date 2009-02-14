ee688331251929310f8e969321e5002b0286a682
Michal Czyz <michalczyz@gmail.com>
2009-02-15 00:26:19 +0100

    adding scenarios

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,10 +1,21 @@
 module FeaturesHelper
-
+  PLUS_IMG = "image_tag('feature/plus_12x12.png')"
+  MINUS_IMG = "image_tag('feature/minus_12x12.png')"
   def plus_link_to url, options= {}
-    link_to image_tag('feature/plus_12x12.png'), url, options
+    link_to eval(PLUS_IMG), url, options
   end
 
   def minus_link_to url, options= {}
-    link_to image_tag('feature/minus_12x12.png'), url, options
+    link_to eval(MINUS_IMG), url, options
   end
+  
+  def plus_link_to_function url, options= {}
+    link_to_function eval(PLUS_IMG), url, options
+  end
+
+  def minus_link_to_function url, options= {}
+    link_to_function eval(MINUS_IMG), url, options
+  end
+
+
 end
