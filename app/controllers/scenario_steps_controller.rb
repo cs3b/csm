5f14a03e9ba2a8b6fd88c36c6ecd3c8f64bf6861
Michal Czyz <michalczyz@gmail.com>
2009-02-15 13:01:30 +0100

    cleanup improvements in scenario creation translation

--- app/controllers/scenario_steps_controller.rb
+++ app/controllers/scenario_steps_controller.rb
@@ -1,14 +1,6 @@
 class ScenarioStepsController < ApplicationController
-  before_filter :get_scenario, :only => [:new, :create]
   before_filter :get_step, :only => [:destroy]
-  def new
-    @step = ScenarioStep.new
-  end
-  def create
-    @step = @scenario.steps.build(params[:scenario_step])
-    @step.save
-    redirect_to(@scenario.feature)
-  end
+
   def destroy
     feature = @step.scenario.feature
     @step.destroy
@@ -17,9 +9,6 @@ class ScenarioStepsController < ApplicationController
 
   private
   
-  def get_scenario
-    @scenario = Scenario.find(params[:scenario_id])
-  end
   def get_step
     @step = ScenarioStep.find(params[:id])
   end
