6aee51ece3e73ff7bfe316be5f7ce85af9702667
Michal Czyz <michalczyz@gmail.com>
2009-02-17 21:57:20 +0100

    i18n automatic translation of label

--- vendor/rails/actionpack/lib/action_view/helpers/form_helper.rb
+++ vendor/rails/actionpack/lib/action_view/helpers/form_helper.rb
@@ -693,7 +693,12 @@ module ActionView
         add_default_name_and_id(name_and_id)
         options.delete("index")
         options["for"] ||= name_and_id["id"]
-        content = (text.blank? ? nil : text.to_s) || method_name.humanize
+        if text.blank?
+          content = object.class.respond_to?(:human_attribute_name) ? object.class.human_attribute_name(method_name) : method_name.humanize
+        else
+          content = text.to_s
+        end
+        #content = (text.blank? ? nil : text.to_s) ||object_name.classify.constantize.human_attribute_name(method_name)
         label_tag(name_and_id["id"], content, options)
       end
 
