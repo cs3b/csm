fcb6758528bdde7322d0e99abc16c2b891b856a7
Michal Czyz <mczyz@linus-desktop.(none)>
2009-02-19 19:44:39 +0100

    added translation & improvement in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '46bd7f266810096aa04e4699e21111da'
+#  protect_from_forgery  :secret => '46bd7f266810096aa04e4699e21111da'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
